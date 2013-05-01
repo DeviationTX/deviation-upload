5309ab3cc3d3762745114d6aa45b1b16c4fe918c
PhracturedBlue <rc2012@pblue.org>
2013-04-30 20:30:42 -0700

    Add enscryption code

--- src/deviation/DeviationInfo.java
+++ src/deviation/DeviationInfo.java
@@ -34,7 +34,7 @@ public class DeviationInfo {
     public long id1() { return id1;}
     public long id2() { return id2;}
     public long id3() { return id3;}
-    private byte[] convertToByteArray(long[] data) {
+    private byte[] convertToByteArray(long[] data, int offset) {
         byte []buf = new byte[16];
         int i = 0;
         for (long  val : data) {
@@ -43,39 +43,48 @@ public class DeviationInfo {
             buf[i++] = (byte)((val >> 16) & 0xff);
             buf[i++] = (byte)((val >> 24) & 0xff);
         }
+        for (i = 0; i < buf.length; i++) {
+            int val = buf[i] & 0xff;
+            if(val >= 0x80 && val <= 0xcf - offset) {
+                val += offset;
+            } else if(val >= 0xd0 - offset && val < 0xd0) {
+                val -= (0x50 - offset);
+            }
+            buf[i] = (byte)val;
+        }
         return buf;
     }
     private byte[] encode_7e() {
         long[] a = new long[4];
-        a[0] = 0xFFFFFFFF & (id1 + id2);
-        a[1] = 0xFFFFFFFF & (id2 ^ id2);
-        a[2] = 0xFFFFFFFF & (id1 + id3);
-        a[3] = 0xFFFFFFFF & (id2 ^ id3);
-        return convertToByteArray(a);
+        a[0] = 0xFFFFFFFFL & (id1 + id2);
+        a[1] = 0xFFFFFFFFL & (id2 ^ id2);
+        a[2] = 0xFFFFFFFFL & (id1 + id3);
+        a[3] = 0xFFFFFFFFL & (id2 ^ id3);
+        return convertToByteArray(a, 7);
     }
     private byte[] encode_6_8() {
         long[] a = new long[4];
-        a[0] = 0xFFFFFFFF & (id1 + id2);
-        a[1] = 0xFFFFFFFF & (id1 ^ id2);
-        a[2] = 0xFFFFFFFF & (id1 + id3);
-        a[3] = 0xFFFFFFFF & (id1 ^ id3);
-        return convertToByteArray(a);
+        a[0] = 0xFFFFFFFFL & (id1 + id2);
+        a[1] = 0xFFFFFFFFL & (id1 ^ id2);
+        a[2] = 0xFFFFFFFFL & (id1 + id3);
+        a[3] = 0xFFFFFFFFL & (id1 ^ id3);
+        return convertToByteArray(a, 8);
     }
     private byte[] encode_10() {
         long[] a = new long[4];
-        a[0] = 0xFFFFFFFF & (id1 + id2);
-        a[1] = 0xFFFFFFFF & (id1 ^ id2);
-        a[2] = 0xFFFFFFFF & (id2 + id3);
-        a[3] = 0xFFFFFFFF & (id2 ^ id3);
-        return convertToByteArray(a);
+        a[0] = 0xFFFFFFFFL & (id1 + id2);
+        a[1] = 0xFFFFFFFFL & (id1 ^ id2);
+        a[2] = 0xFFFFFFFFL & (id2 + id3);
+        a[3] = 0xFFFFFFFFL & (id2 ^ id3);
+        return convertToByteArray(a, 10);
     }
     private byte[] encode_12() {
         long[] a = new long[4];
-        a[0] = 0xFFFFFFFF & (id1 + id2 + id3);
-        a[1] = 0xFFFFFFFF & (id1 ^ id2 ^ id3);
-        a[2] = 0xFFFFFFFF & (id2 + id3);
-        a[3] = 0xFFFFFFFF & (id2 ^ id3);
-        return convertToByteArray(a);
+        a[0] = 0xFFFFFFFFL & (id1 + id2 + id3);
+        a[1] = 0xFFFFFFFFL & (id1 ^ id2 ^ id3);
+        a[2] = 0xFFFFFFFFL & (id2 + id3);
+        a[3] = 0xFFFFFFFFL & (id2 ^ id3);
+        return convertToByteArray(a, 12);
     }
     public byte [] encodeId() {
         switch (type) {
--- src/deviation/DeviationUploader.java
+++ src/deviation/DeviationUploader.java
@@ -6,16 +6,31 @@ import de.ailis.usb4java.libusb.*;
 
 public class DeviationUploader
 {
+    public static byte[] applyEncryption(DfuFile.ImageElement elem, DeviationInfo info)
+    {
+        long address = elem.address();
+        byte[] data = elem.data();
+        if (address < 0x08005000L && address + data.length > 0x08005000L) {
+            int offset = (int)(0x08005000L - address);
+            byte[] txcode = info.encodeId();
+            for (byte b : txcode) {
+                data[offset++] = b;
+            }
+        }
+        return data;
+    }
+    public static DfuDevice find_device(List<DfuDevice> devs, int vid, int pid, int alt)
+    {
+        for (DfuDevice dev : devs) {
+            if (vid == dev.idVendor() && pid == dev.idProduct() && alt == dev.bAlternateSetting()) {
+                return dev;
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args)
     {
-        try {
-            byte[] data = IOUtil.readFile("devo8.dfu");
-            DfuFile f = new DfuFile(data);
-            return;
-        } catch (IOException e) {
-            System.err.println("Caught IOException: " + e.getMessage());
-            //throw new RuntimeException(e);
-        }
         DeviceList devices = new DeviceList();
         LibUsb.init(null);
         LibUsb.getDeviceList(null, devices);
@@ -31,24 +46,39 @@ public class DeviationUploader
                 dev.Memory().name());
             //DfuFuncDescriptor desc = new DfuFuncDescriptor(dev);
         }
-        int idx = 0;
-        for (DfuDevice dev : devs) {
+
+        DfuFile file;
+        try {
+            byte[] fname = IOUtil.readFile("devo8.dfu");
+            file = new DfuFile(fname);
+        } catch (IOException e) {
+            System.err.println("Caught IOException: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+        for (DfuFile.ImageElement elem : file.imageElements()) {
+            DfuDevice dev = find_device(devs, file.idVendor(), file.idProduct(), elem.altSetting());
+            if (dev == null) {
+                System.out.format("Error: Did not find matching device for VID:0x%x PID:0x%x alt:%d%n",
+                    file.idVendor(), file.idProduct(), elem.altSetting());
+                continue;
+            }
             if (dev.open() != 0) {
                 System.out.println("Error: Unable to open device");
                 break;
             }
             dev.claim_and_set();
             Dfu.setIdle(dev);
-            byte [] data = Dfu.FetchFromDevice(dev, 0x08000400, 0x40);
-            DeviationInfo info = new DeviationInfo(data);
-            System.out.format("%s : %x %x %x%n", info.model(), info.id1(), info.id2(), info.id3());
-/*
+            byte [] txInfo = Dfu.FetchFromDevice(dev, 0x08000400, 0x40);
+            DeviationInfo info = new DeviationInfo(txInfo);
+            byte [] data = applyEncryption(elem, info);
+            dev.close();
+
             try{
-                File file = new File("output" + Integer.toString(idx) + ".txt");
-                FileOutputStream fop = new FileOutputStream(file);
+                File f = new File("output.txt");
+                FileOutputStream fop = new FileOutputStream(f);
                 // if file doesnt exists, then create it
-                if (!file.exists()) {
-                    file.createNewFile();
+                if (!f.exists()) {
+                    f.createNewFile();
                 }
                 fop.write(data);
                 fop.flush();
@@ -56,10 +86,7 @@ public class DeviationUploader
             } catch (IOException e) {
                 e.printStackTrace();
             }
-*/
-            dev.close();
-            idx++;
-            break;
+
         }
         LibUsb.freeDeviceList(devices, true);
         LibUsb.exit(null);
