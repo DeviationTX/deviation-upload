0e8db668908e526d76e002c90ece141609f00c70
nitram509 <maki@bitkings.de>
2019-03-06 09:11:16 +0100

    move tests into its own test folder, as it is common practice in Java projects simply run ```mvn test``` to fire the whole test suite at once

--- src/deviation/DeviationUploader.java
+++ src/deviation/DeviationUploader.java
@@ -358,64 +358,7 @@ public class DeviationUploader
         }
         return null;
     }
-    public static void test() {
-        try {
-            BlockDevice bd = new FileDisk(new File("test.fat"), false);
-            FileSystem fs = FatFileSystem.read(bd, false);
-            FileGroup zips = new FileGroup();
-            zips.AddFile("test.zip");
-            for (FileInfo file: zips.GetFilesystemFiles()) {
-            	FSUtils.copyFile(fs, file);
-            }
-            fs.close();
-            bd.close();
-            /*
-            String[]dirs = "/media/".split("/");
-            FsDirectory dir = fs.getRoot();
-            for (String subdir : dirs) {
-                if (subdir.equals("")) {
-                   continue;
-                }
-                dir = dir.getEntry(subdir).getDirectory();
-            }
-            Iterator<FsDirectoryEntry> itr = dir.iterator();
-            while(itr.hasNext()) {
-                FsDirectoryEntry entry = itr.next();
-                System.out.println(entry.getName());
-            }
-            */
-        } catch (Exception e) { e.printStackTrace();  }
-        System.exit(0);
 
-    }
-    private static void test1_recur(String indent, FsDirectory dir) {
-        Iterator<FsDirectoryEntry> itr = dir.iterator();
-    	while(itr.hasNext()) {
-    		try {
-    			FsDirectoryEntry entry = itr.next();
-    			if (entry.isDirectory()) {
-    				System.out.format("%sDIR: %s\n", indent, entry.getName());
-    				test1_recur(indent + "    ", entry.getDirectory());
-    			} else {
-    				System.out.format("%sFILE: %s (%d)\n", indent, entry.getName(), entry.getFile().getLength());
-    			}
-    		} catch (Exception e) { e.printStackTrace(); }
-    	}
-    }
-    public static void test1() {
-    	try {
-    		FileDisk2 f = new FileDisk2(new File("test.devofs"), false, 4096);
-    		DevoFSFileSystem fs = new DevoFSFileSystem(f, false);
-            FsDirectory dir = fs.getRoot();
-            test1_recur("", dir);
-            ByteBuffer dest = ByteBuffer.allocate((int)f.getSize());
-            Arrays.fill(dest.array(), (byte)0);
-            f.write(0, dest);
-            fs.close();
-    	} catch (Exception e) { e.printStackTrace(); }    
-     	System.exit(0);
-    }
-    
     public static void main(String[] args)
     {
         if (args.length == 0) {
